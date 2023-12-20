0e1b14deba138739ef186671450c712d6884f942
Andres Saldarriaga Gaviria <jandreus87@gmail.com>
2023-12-20 09:48:44 -0500

    added redux-thunk

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,14 +1,17 @@
 import ProductCard from "../product-card/product-card.component";
 
-import { CategoryPreviewContainer, Title, Preview } from "./category-preview.styles.jsx";
+import {
+    CategoryPreviewContainer,
+    Title,
+    Preview,
+} from "./category-preview.styles.jsx";
 
 const CategoryPreview = ({ title, products }) => {
-
     return (
         <CategoryPreviewContainer>
             <h2>
-                <Title to={title} >{title.toUpperCase()}</Title>
-            </h2> 
+                <Title to={title}>{title.toUpperCase()}</Title>
+            </h2>
             <Preview>
                 {products
                     .filter((_, idx) => idx < 4)
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 
 import "./shop.styles.scss";
 
@@ -13,12 +12,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMaps = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray));
-        };
-
-        getCategoriesMaps();
+        dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
